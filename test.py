cff68de805fd8506010a0db402ef0f5874dac6dc
Abhishek Bhattacharjee <abhishek.bhattacharjee11@gmail.com>
2014-04-06 03:52:04 +0530

    Added files and suite.

--- test.py
+++ test.py
@@ -13,10 +13,10 @@ cur = conn.cursor()
 
 #cur.execute("select bin,s.suite_name from bin_associations b, suite s where b.suite = s.id")
 
-sql2='''select b.package,b.version, s.source, a.arch_string 
-        from binaries b, architecture a, source s
+sql2='''select b.package,b.version, s.source, a.arch_string, su.suite_name, f.filename
+        from binaries b, architecture a, source s, bin_associations ba,suite su, files f
         where
-        b.architecture = a.id and s.id = b.source;'''
+        b.architecture = a.id and s.id = b.source and b.id = ba.bin and su.id = ba.suite and f.id = b.file;'''
 
 cur.execute(sql2)
 
@@ -24,7 +24,7 @@ res = cur.fetchall()
 ofile = open("Components.yml","w")
 
 for r in res:
-    dic = {'Package':r[0],'Version':r[1],'Source':r[2],'Arch':r[3]}
+    dic = {'Package':r[0],'Version':r[1],'Source':r[2],'Arch':r[3],'Suite':r[4],'File':r[5]}
     data = yaml.dump(dic,default_flow_style=False,explicit_start=True)
     ofile.write(data)
     ofile.write("\n")
