18dac2f3d3fb0e9e44ef228f00a1cce5255cd94e
Abhishek Bhattacharjee <abhishek.bhattacharjee11@gmail.com>
2014-04-06 14:14:56 +0530

    Added Components field.

--- test.py
+++ test.py
@@ -6,25 +6,31 @@ import yaml
 
 conn = psycopg2.connect(host="localhost",database="newdakdb",user="postgres",password="postgres")
 cur = conn.cursor()
-#cur.execute("select * from binaries_metadata")
-#cur.execute("select  id,package,type from binaries")
 
-#cur.execute("select * from files")
-
-#cur.execute("select bin,s.suite_name from bin_associations b, suite s where b.suite = s.id")
-
-sql2='''select b.package,b.version, s.source, a.arch_string, su.suite_name, f.filename
-        from binaries b, architecture a, source s, bin_associations ba,suite su, files f
+sql_str1='''select b.package,b.version, s.source, a.arch_string, su.suite_name, f.filename, ar.name, su.id
+        from binaries b, architecture a, source s, bin_associations ba,suite su, files f, archive ar
         where
-        b.architecture = a.id and s.id = b.source and b.id = ba.bin and su.id = ba.suite and f.id = b.file;'''
+        b.architecture = a.id and s.id = b.source and b.id = ba.bin and su.id = ba.suite and f.id = b.file and ar.id = su.archive_id'''
 
-cur.execute(sql2)
+cur.execute(sql_str1)
 
 res = cur.fetchall()
 ofile = open("Components.yml","w")
 
 for r in res:
-    dic = {'Package':r[0],'Version':r[1],'Source':r[2],'Arch':r[3],'Suite':r[4],'File':r[5]}
+    dic = {'Package':r[0],'Version':r[1],'Source':r[2],'Arch':r[3],'Suite':r[4],'File':r[5],'Archive':r[6]}
+    ##Query is used to get the Component field##
+    sql_str2 = '''select c.name 
+              from  component_suite cs, component c 
+              where 
+              cs.suite_id = %s and cs.component_id = c.id;'''%(r[7])
+    cur.execute(sql_str2)
+    comp = cur.fetchall()
+    comp_str = ""
+    for c in comp:
+        comp_str = comp_str + c[0] + " "
+
+    dic['Component']=comp_str
     data = yaml.dump(dic,default_flow_style=False,explicit_start=True)
     ofile.write(data)
     ofile.write("\n")
