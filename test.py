1423e7b92fa848790920b0a6fec46562422b0282
Abhishek Bhattacharjee <abhishek.bhattacharjee11@gmail.com>
2014-04-12 15:01:29 +0530

    Ported to sqlalchemy. Latest packages to be fetched remaining.

--- test.py
+++ test.py
@@ -1,43 +1,44 @@
 #!/usr/bin/python
 
-import psycopg2
 import pprint
 import yaml
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-conn = psycopg2.connect(host="localhost",database="newdakdb",user="postgres",password="postgres")
-cur = conn.cursor()
+engine = create_engine('postgresql+psycopg2://postgres:postgres@localhost/newdakdb')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 sql_str1='''select b.package,b.version, s.source, a.arch_string, su.suite_name, f.filename, ar.name, su.id, ar.path
         from binaries b, architecture a, source s, bin_associations ba,suite su, files f, archive ar
         where
         b.architecture = a.id and s.id = b.source and b.id = ba.bin and su.id = ba.suite and f.id = b.file and ar.id = su.archive_id'''
 
-cur.execute(sql_str1)
-
-res = cur.fetchall()
-ofile = open("Components.yml","w")
+result = session.execute(sql_str1)
+res = result.fetchall()
+ofile = open("components.yml","w")
 
 for r in res:
-    dic = {'Package':r[0],'Version':r[1],'Source':r[2],'Arch':r[3],'Suite':r[4],'File':r[5],'Archive':r[6],'Path':r[8]}
+    dic = {'Package':str(r[0]),'Version':str(r[1]),'Source':str(r[2]),'Arch':str(r[3]),'Suite':str(r[4]),'File':str(r[5]),'Archive':str(r[6]),'Path':str(r[8])}
+
     ##Query is used to get the Component field##
     sql_str2 = '''select c.name 
               from  component_suite cs, component c 
               where 
-              cs.suite_id = %s and cs.component_id = c.id;'''%(r[7])
-    cur.execute(sql_str2)
-    comp = cur.fetchall()
+              cs.suite_id = %s and cs.component_id = c.id;'''%(str(r[7]))
+    comp=session.execute(sql_str2)
     comp_str = ""
     for c in comp:
-        comp_str = comp_str + c[0] + " "
+        comp_str = comp_str + str(c[0]) + " "
 
-    dic['Component']=comp_str
+    dic['Component']=comp_str.rstrip()
+    
     data = yaml.dump(dic,default_flow_style=False,explicit_start=True)
     ofile.write(data)
     ofile.write("\n")
-
-conn.close()
+    
+session.flush()
 ofile.close()
 
 #pprint.pprint(res)
 #print res
-
